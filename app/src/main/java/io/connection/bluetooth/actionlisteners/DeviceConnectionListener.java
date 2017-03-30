5b38f554e930f88375b59387c3189819cf3e9d60
Kinjal Patel <KP49107@zensar.com>
2017-03-30 18:42:19 +0530

    wifdirect socket connection

---
+++ app/src/main/java/io/connection/bluetooth/actionlisteners/DeviceConnectionListener.java
@@ -0,0 +1,10 @@
+package io.connection.bluetooth.actionlisteners;
+
+import android.net.wifi.p2p.WifiP2pDevice;
+
+/**
+ * Created by KP49107 on 30-03-2017.
+ */
+public interface DeviceConnectionListener {
+    public void onDeviceConnected(boolean isConnected);
+}
